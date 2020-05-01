8b32cd7dd076e6fee9e3f84b6dac6ea47c4898a8
fabio scholl <fabio.scholl@hotmail.com>
2020-05-01 01:40:56 -0300

    commit to github

---
+++ README.md
@@ -0,0 +1,20 @@
+This application web project was a personal study about Python and Django. 
+All reference material can be found at MDN Web Docs Website:
+Access <https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Django>
+
+## Configuration:
+A json doc must be created within project dir level and the name must be <config.json>. On the other hand, instead
+settings.py correction must be done.
+
+- At <config.json>: <br/>
+{<br/>
+    "SECRET_KEY": "",
+    "EMAIL_USE_TLS": "",
+    "EMAIL_HOST": "",
+    "EMAIL_HOST_USER": "",
+    "EMAIL_HOST_PASSWORD": "",
+    "EMAIL_PORT": ,
+    "AWS_ACCESS_KEY_ID": "",
+    "AWS_SECRET_ACCESS_KEY": "",
+    "AWS_STORAGE_BUCKET_NAME": ""<br/>
+}
---
+++ catalog/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('change-password/', auth_views.PasswordChangeView.as_view()),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('allbooks-borrowed/', views.LibraryView.as_view(), name='all-borrowed'),
+    path('email/', views.send_email, name='send_email'),
+]
+
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+    path('book/create/', views.BookCreate.as_view(), name='book_create'),
+]
