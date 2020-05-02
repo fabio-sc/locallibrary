e297b03aab9c20d8500b9bbacc95d3a260c27974
fabio scholl <fabio.scholl@hotmail.com>
2020-05-01 21:01:15 -0300

    settings STATIC_ROOT

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -126,10 +126,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static"),
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
